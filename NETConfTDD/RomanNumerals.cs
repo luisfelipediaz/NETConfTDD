4623cae6ca224e42a2c37d203c0a7da66872c4e4
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2018-10-24 10:44:50 -0500

    Reestructure example

--- NETConfTDD/RomanNumerals.cs
+++ NETConfTDD/RomanNumerals.cs
@@ -8,24 +8,25 @@ namespace NETConfTDD
         public static string ArabicToRoman(int number)
         {
             StringBuilder romanNumber = new StringBuilder();
-            if (number == 6)
-            {
-                romanNumber.Append("VI");
-            }
-            else if (number == 5)
-            {
-                romanNumber.Append("V");
-            }
-            else if (number == 4)
-            {
-                romanNumber.Append("IV");
-            }
-            else
+
+            if (number <= 3)
             {
                 for (int i = 0; i < number; i++)
                 {
                     romanNumber.Append("I");
                 }
+            }
+            else if (number == 4)
+            {
+                romanNumber.Append("IV");
+            }
+            else if (number == 5)
+            {
+                romanNumber.Append("V");
+            }
+            else
+            {
+                romanNumber.Append("VI");
             }
 
             return romanNumber.ToString();
