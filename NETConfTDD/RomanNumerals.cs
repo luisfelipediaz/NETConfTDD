85eced428cc879f9d45d6426f888a8e93b25fffa
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2018-10-29 23:18:05 -0500

    Refactor

--- NETConfTDD/RomanNumerals.cs
+++ NETConfTDD/RomanNumerals.cs
@@ -8,6 +8,20 @@ namespace NETConfTDD
         public static string ArabicToRoman(int number)
         {
             StringBuilder romanNumber = new StringBuilder();
+            ConvertDigit(number, romanNumber);
+
+            if (number >= 10)
+            {
+                romanNumber.Append("X");
+                number -= 10;
+                ConvertDigit(number, romanNumber);
+            }
+
+            return romanNumber.ToString();
+        }
+
+        private static void ConvertDigit(int number, StringBuilder romanNumber)
+        {
             if (number <= 3)
                 for (int i = 0; i < number; i++)
                     romanNumber.Append("I");
@@ -21,27 +35,6 @@ namespace NETConfTDD
             }
             else if (number == 9)
                 romanNumber.Append("IX");
-
-            if (number >= 10)
-            {
-                romanNumber.Append("X");
-                number -= 10;
-                if (number <= 3)
-                    for (int i = 0; i < number; i++)
-                        romanNumber.Append("I");
-                else if (number == 4)
-                    romanNumber.Append("IV");
-                else if (number >= 5 && number <= 8)
-                {
-                    romanNumber.Append("V");
-                    for (int i = 5; i < number; i++)
-                        romanNumber.Append("I");
-                }
-                else if (number == 9)
-                    romanNumber.Append("IX");
-            }
-
-            return romanNumber.ToString();
         }
     }
 }
