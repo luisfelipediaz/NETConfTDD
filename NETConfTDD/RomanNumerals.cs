719af617f55dee35a9568ee58589f4d473bd57b4
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2018-10-29 22:58:41 -0500

    Refactor
    se hace un refactor que hace evidente el patro del numero 1 hasta el 9

--- NETConfTDD/RomanNumerals.cs
+++ NETConfTDD/RomanNumerals.cs
@@ -9,47 +9,36 @@ namespace NETConfTDD
         {
             StringBuilder romanNumber = new StringBuilder();
             if (number <= 3)
-            {
                 for (int i = 0; i < number; i++)
-                {
                     romanNumber.Append("I");
-                }
-            }
             else if (number == 4)
-            {
                 romanNumber.Append("IV");
-            }
             else if (number >= 5 && number <= 8)
             {
                 romanNumber.Append("V");
                 for (int i = 5; i < number; i++)
-                {
                     romanNumber.Append("I");
-                }
             }
             else if (number == 9)
-            {
                 romanNumber.Append("IX");
-            }
-            else if (number >= 10 && number <= 13)
+
+            if (number >= 10)
             {
                 romanNumber.Append("X");
-                for (int i = 10; i < number; i++)
+                number -= 10;
+                if (number <= 3)
+                    for (int i = 0; i < number; i++)
+                        romanNumber.Append("I");
+                else if (number == 4)
+                    romanNumber.Append("IV");
+                else if (number >= 5 && number <= 8)
                 {
-                    romanNumber.Append("I");
+                    romanNumber.Append("V");
+                    for (int i = 5; i < number; i++)
+                        romanNumber.Append("I");
                 }
-            }
-            else if (number == 14)
-            {
-                romanNumber.Append("XIV");
-            }
-            else if (number >= 15 && number <= 18)
-            {
-                romanNumber.Append("XV");
-                for (int i = 15; i < number; i++)
-                {
-                    romanNumber.Append("I");
-                }
+                else if (number == 9)
+                    romanNumber.Append("IX");
             }
 
             return romanNumber.ToString();
